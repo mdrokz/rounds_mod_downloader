e3eaae37784be677091ff7b5629d62a47b3c8618
mdrokz <mohammadmunshi@gmail.com>
2022-05-12 01:45:22 +0530

    implement clap for CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{env::args, fs, os::unix::fs::DirEntryExt, path::Path};
+
+use clap::{App, Arg, Command};
+
+use resolve_path::PathResolveExt;
+
 fn main() {
-    println!("Hello, world!");
+    const CONFIG_PATH: &str = "~/.config/rounds_mod_downloader";
+
+    let config_path = CONFIG_PATH
+        .try_resolve()
+        .expect("couldnt resolve home path");
+
+    if !config_path.is_dir() {
+        std::fs::create_dir(
+            CONFIG_PATH
+                .try_resolve()
+                .expect("couldnt resolve home path"),
+        )
+        .expect("couldnt create config directory");
+    }
+
+    let plugin_path = args().next().expect("please provide your plugin path");
+
+    let matches = Command::new("Rounds Mod Downloader")
+        .version("1.0")
+        .about("CLI for downloading mods from thunderstore.io")
+        .subcommand(
+            Command::new("download")
+                .about("Download mod by providing a mod_id")
+                .arg(
+                    Arg::new("mod_id")
+                        .short('i')
+                        .value_name("MOD_ID")
+                        .help("provide a mod_id to download the mod"),
+                ),
+        )
+        .get_matches();
 }
