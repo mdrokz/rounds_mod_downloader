3b4903898082a19134b38bc26211fc9ae32042e9
mdrokz <mohammadmunshi@gmail.com>
2022-05-12 14:35:53 +0530

    switch to struct based API for clap

--- src/args.rs
+++ src/args.rs
@@ -1,1 +1,38 @@
-use clap::{Args, Command, SubCommand};
+use std::{borrow::Cow, path::Path};
+
+use clap::{Args, Command, Parser, Subcommand};
+use resolve_path::PathResolveExt;
+
+#[derive(Debug, Parser)]
+#[clap(author = "mdrokz", version = "1.0", about)]
+pub struct RoundsArgs {
+    #[clap(subcommand)]
+    pub entity_type: EntityType,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum EntityType {
+    Download(DownloadCommand),
+}
+
+pub struct ResolveDownload;
+
+const ROUNDS_PATH: &'static str = "~/.steam/steam/steamapps/common/ROUNDSs";
+
+impl ResolveDownload {
+    fn resolve() -> &'static str {
+        let _ = ROUNDS_PATH
+            .try_resolve()
+            .expect("unable to resolve home path");
+
+        ROUNDS_PATH
+    }
+}
+
+#[derive(Debug, Args)]
+pub struct DownloadCommand {
+    #[clap(default_value = ResolveDownload::resolve(),short, long)]
+    pub download_path: String,
+    #[clap(short, long)]
+    pub mod_id: String,
+}
