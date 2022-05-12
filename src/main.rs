8c0a113d3f2ef0d9ef95cf9f9d0caa3e8c467c73
mdrokz <mohammadmunshi@gmail.com>
2022-05-12 15:18:12 +0530

    implement http client

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod args;
 
 fn main() {
     const CONFIG_PATH: &str = "~/.config/rounds_mod_downloader";
+    const DOWNLOAD_URL: &str = "https://rounds.thunderstore.io/package/download";
 
     let config_path = CONFIG_PATH
         .try_resolve()
@@ -28,7 +29,38 @@ fn main() {
 
     match args.entity_type {
         args::EntityType::Download(c) => {
-            println!("{:?}",c);
-        },
+            let (download_path, mod_id) = (
+                c.download_path
+                    .try_resolve()
+                    .expect("failed to resolve home path"),
+                c.mod_id,
+            );
+
+            if !download_path.is_dir() {
+                panic!("Provided directory {:?} does not exist", download_path);
+            }
+
+            let mod_resp = attohttpc::get(format!("{}/{}", DOWNLOAD_URL, mod_id))
+                .send()
+                .expect("failed to download mod");
+
+            let headers = mod_resp.headers();
+
+            let file_type = headers
+                .get("content-type")
+                .expect("failed to get content-type")
+                .to_str()
+                .and_then(|x| {
+                    let content_type = String::from(x);
+                    let mut c_split = content_type.split("/");
+                    let ctype = c_split.next().expect("failed to get value");
+                    Ok(ctype.to_string())
+                })
+                .expect("failed to convert to string");
+
+            
+
+            println!("{:?}", mod_resp.headers());
+        }
     }
 }
