7e3a809706e597506dbf7ffffec4ed3dd234406a
mdrokz <mohammadmunshi@gmail.com>
2022-05-21 01:17:43 +0530

    remove config

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,17 @@
 #![feature(drain_filter)]
 
-
-use clap::{Parser};
+use clap::Parser;
 
 use resolve_path::PathResolveExt;
 
-use crate::{args::RoundsArgs};
+use crate::args::RoundsArgs;
 
 mod args;
 
 mod types;
 
 fn main() {
-    const CONFIG_PATH: &str = "~/.config/rounds_mod_downloader";
     const DOWNLOAD_URL: &str = "https://rounds.thunderstore.io/package/download";
-
-    let config_path = CONFIG_PATH
-        .try_resolve()
-        .expect("couldnt resolve home path");
-
-    if !config_path.is_dir() {
-        std::fs::create_dir(
-            CONFIG_PATH
-                .try_resolve()
-                .expect("couldnt resolve home path"),
-        )
-        .expect("couldnt create config directory");
-    }
 
     let args = RoundsArgs::parse();
 
